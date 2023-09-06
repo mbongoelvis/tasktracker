3312299b933ccb1ad9e4aac77364f8a937487141
mbongoelvis <mbongombongo4@gmail.com>
2023-09-06 14:43:03 +0100

    second commit

--- script.js
+++ script.js
@@ -24,56 +24,53 @@ const addTask = () => {
 
 // call back
 const savetask = () => { 
-  // checkbox
-  
-    if (taskname.value === "") {
-      alert("A field can not be empty");
-    } else {
-      // creating the elements
-      const addedtaskDiv = document.createElement("div");
-      const firstchild = document.createElement("div");
-      const closingicon = document.createElement("span");
-      const firstchildren = document.createElement("p");
-      const secondchildren = document.createElement("p");
+  if (taskname.value === "") {
+    alert("A field can not be empty");
+  } else {
+    // creating the elements
+    const addedtaskDiv = document.createElement("div");
+    const firstchild = document.createElement("div");
+    const closingicon = document.createElement("span");
+    const firstchildren = document.createElement("p");
+    const secondchildren = document.createElement("p");
 
-      // adding texts to the elements
-      firstchildren.textContent = taskname.value;
-      secondchildren.textContent = dayandtime.value
+    // adding texts to the elements
+    firstchildren.textContent = taskname.value;
+    secondchildren.textContent = dayandtime.value
 
-      // appending the newly created elements
-      addedtaskDiv.appendChild(firstchild);
-      addedtaskDiv.appendChild(closingicon);
-      firstchild.appendChild(firstchildren);
-      firstchild.appendChild(secondchildren);
-      newtask.appendChild(addedtaskDiv);
+    // appending the newly created elements
+    addedtaskDiv.appendChild(firstchild);
+    addedtaskDiv.appendChild(closingicon);
+    firstchild.appendChild(firstchildren);
+    firstchild.appendChild(secondchildren);
+    newtask.appendChild(addedtaskDiv);
 
-      // adding the styles to each of the created elements
-      addedtaskDiv.classList.add(
-        "flex",
-        "justify-between",
-        "mt-2",
-        "items-center",
-        "bg-gray-100",
-        "px-4",
-        "py-2",
-        "rounded",
-        "border-green-500",
-        "cursor-pointer",
-        "doubleclicking"
-      );
-      firstchildren.classList.add("font-medium");
-      closingicon.classList.add(
-        "material-symbols-outlined", "text-red-500", "font-bold", "cursor-pointer");
-      closingicon.innerHTML = "close"
+
+    // checkbox section
+    if (checkbox.checked) {
+      addedtaskDiv.classList.add("border-l-4");
     }
+
+    // adding the styles to each of the created elements
+    addedtaskDiv.classList.add(
+      "flex",
+      "justify-between",
+      "mt-2",
+      "items-center",
+      "bg-gray-100",
+      "px-4",
+      "py-2",
+      "rounded",
+      "border-green-500",
+      "cursor-pointer",
+      "doubleclicking"
+    );
+    firstchildren.classList.add("font-medium");
+    closingicon.classList.add(
+      "material-symbols-outlined", "text-red-500", "font-bold", "cursor-pointer");
+    closingicon.innerHTML = "close"
+    localStorage.setItem("task", addedtaskDiv.innerHTML);
+  }
   taskname.value = "";
   dayandtime.value = "";
-}
-try {
-  addedtaskDiv.addEventListener("dblclick", (e) => {
-    // Double-click event handler
-    console.log("Double-clicked!");
-  });
-} catch (err) {
-  console.log(err);
 }
